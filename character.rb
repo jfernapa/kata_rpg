d53d7b56a46f8c6ba84e606514bb885be4dd86e2
jfernapa <jfernapa@gmail.com>
2016-08-02 23:53:22 +0200

    Refactoring and renaming files

--- character.rb
+++ character.rb
@@ -2,6 +2,8 @@ class Character
     INITIAL_HEALTH_POINTS = 1000
     INITIAL_LEVEL = 1
     FIGHTER_TYPES = {melee: 2, ranged: 20}
+    REDUCTION_FACTOR = 0.5
+    MAGNIFICATION_FACTOR = 1.5
 
     attr_accessor :level, :factions_joined
     attr_reader :health, :range, :position, :factions_joined
@@ -16,26 +18,34 @@ class Character
     end
 
     def attack(target, damage)
-        if target.class == Character
-            damage *= 0.5 if has_five_or_more_levels_above_attacker?(target)
-            damage += damage * 0.5 if has_five_or_more_levels_below_attacker?(target)
+        if is_a_character?(target) 
+	    damage = damage_modifier(target, damage)
             target.receive_damage(damage) if can_receive_damage?(target)
         else
             target.receive_damage(damage)
         end
     end
-    
+ 
     def heal(target, health_points)
-        if target.class == self.class
-            if target.alive? and (is_an_ally?(target) or am_i_the_target?(target))
-                if (target.health + health_points) > INITIAL_HEALTH_POINTS
-                    health_points = INITIAL_HEALTH_POINTS - target.health
-                end
-                target.receive_health_points(health_points)
+        return unless target.class == self.class
+
+        if target.alive? and (is_an_ally?(target) or am_i_the_target?(target))
+            if (target.health + health_points) > INITIAL_HEALTH_POINTS
+                health_points = INITIAL_HEALTH_POINTS - target.health
             end
+            target.receive_health_points(health_points)
         end
     end
     
+    def damage_modifier(target, damage)
+	fifty_percent_damage = damage * REDUCTION_FACTOR
+	hundred_fifty_percent_damage = damage * MAGNIFICATION_FACTOR
+
+	return fifty_percent_damage if has_five_or_more_levels_above_attacker?(target)
+    	return hundred_fifty_percent_damage if has_five_or_more_levels_below_attacker?(target)
+	return damage
+    end
+
     def receive_damage(damage)
         @alive = false if damage >= @health
         @health -= damage
@@ -89,6 +99,10 @@ class Character
         return (target.position - @position).magnitude <= @range
     end
 
+    def is_a_character?(target)
+        target.class == Character
+    end
+
     def alive?
         @alive
     end
@@ -107,6 +121,8 @@ class Character
             :in_range?,
             :has_five_or_more_levels_below_attacker?,
             :has_five_or_more_levels_above_attacker?,
-            :can_receive_damage?
+            :can_receive_damage?,
+	    :damage_modifier,
+	    :is_a_character?
 
 end
